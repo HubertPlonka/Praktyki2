3e334a573fdb6ed7d5692f42b7e1300c5fcb9e26
Hubert Plonka <plonka.hubert@gmail.com>
2020-09-18 16:09:25 +0200

    Edit IPbus

--- IPBmodule/IPBmodule.srcs/sources_1/new/IPbus_class.sv
+++ IPBmodule/IPBmodule.srcs/sources_1/new/IPbus_class.sv
@@ -7,3 +7,25 @@ interface IPbus_intf(
     );
 endinterface: IPbus_intf;
 
+class IPbus_test;
+
+
+    
+    local virtual IPb_intf intf;
+ 
+    
+    task assign_interface( virtual IPb_intf intf);
+        this.intf = intf;       
+    endtask: assign_interface
+
+    local int x;
+ 
+    
+    task setter(int i);
+        x = i;
+    endtask
+ 
+    
+    function int getter();
+        return x;
+    endfunction
