ffd4f93985db27045da02e8680300b47accc2344
Hubert Plonka <plonka.hubert@gmail.com>
2020-09-23 17:17:22 +0200

    Edit IPbus

---
+++ IPBmodule/IPBmodule.srcs/sim_1/new/IPBus_class_tb.sv
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.09.2020 17:14:21
+// Design Name: 
+// Module Name: IPBus_class_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module IPBus_class_tb(
+    input ipb_in,
+    input ipb_wdata,
+    output ipb_out
+    );
+endmodule
