7c9a25810b6c06e4931d695513edff1f2ea41701
Hubert Plonka <plonka.hubert@gmail.com>
2020-09-23 17:51:39 +0200

    Edit IPbus

--- IPBmodule/IPBmodule.srcs/sim_1/new/IPBus_class_tb.sv
+++ IPBmodule/IPBmodule.srcs/sim_1/new/IPBus_class_tb.sv
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 23.09.2020 17:14:21
+// Create Date: 23.09.2020 17:26:00
 // Design Name: 
-// Module Name: IPBus_class_tb
+// Module Name: IPbus_class_tb
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,9 +20,34 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module IPBus_class_tb(
+module IPbus_class_tb(
     input ipb_in,
-    input ipb_wdata,
     output ipb_out
     );
+endmodule
+
+module tb;
+  reg clk;
+
+  always #10 clk = ~clk;
+  reg_if _if (clk);
+
+  reg_ctrl u0 ( .clk (clk),
+            	.ipb_in (_if.ipb_in),
+               	.ipb_out(_if.ipb_out));
+
+  initial begin
+    new_test t0;
+
+    clk <= 0;
+
+
+    t0 = new;
+    t0.write();
+    t0.read();
+
+  
+    #200 $finish;
+  end
+
 endmodule
